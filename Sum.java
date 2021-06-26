0c7252f9bb60b2a35a977d6a8f8e1f3622b04495
Sourav Kumar Patra <souravkpatra17@gmail.com>
2021-06-26 12:06:27 +0530

    Add Some messgae in sum.java

--- Sum.java
+++ Sum.java
@@ -3,6 +3,6 @@ public class Sum {
     public static void main(String[] args) {
         int a=10;
         int b=50;
-        System.out.println(a+b);
+        System.out.println("Sum Tow Number is: "+a+b);
     }
 }
